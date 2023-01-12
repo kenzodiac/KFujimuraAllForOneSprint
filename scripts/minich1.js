7d57f4ec3812a6e02a46976551348efe82a5a869
kenzodiac <kennethfujimura@gmail.com>
2023-01-12 14:13:36 -0800

    minich1&2 complete

--- scripts/minich1.js
+++ scripts/minich1.js
@@ -1,24 +1,35 @@
+let nameInput = document.getElementById("nameInput");
+let helloReturn = document.getElementById("helloReturn");
+let helloSubBtn = document.getElementById("button-addon2");
+
 let savedInput = "";
 let sayHelloUrl = "";
+
+helloSubBtn.addEventListener("click", function(){
+    helloApi(nameInput);
+});
 
 function urlCall(url){
     fetch(url).then(
         response => response.text()
     ).then(
         data => {
+            helloReturn.textContent = data;
             console.log(data);
-            alert(data);
+            //alert(data);
         }
     );
 }
 
 function helloApi(nameInput){
-    var letters = /^[A-Za-z]+$/;
-    if (nameInput.match(letters)){
-        savedInput = nameInput;
+    var letters = /^[a-zA-Z\s-]+$/;
+    if (nameInput.value.match(letters)){
+        savedInput = nameInput.value;
         sayHelloUrl = "https://kfujimuraallforonesprintapi.azurewebsites.net/MiniCh1/Hello/" + savedInput;
         urlCall(sayHelloUrl);
+    } else {
+        helloReturn.textContent = "Enter a valid response";
     }
 }
 
-helloApi('Kenny');
+//helloApi('Kenny');
--- scripts/minich2.js
+++ scripts/minich2.js
@@ -1,23 +1,38 @@
+let num1Input = document.getElementById("num1Input");
+let num2Input = document.getElementById("num2Input");
+let add2NumsReturn = document.getElementById("add2NumsReturn");
+let add2NumsSubBtn = document.getElementById("submit");
+
 let num1;
 let num2;
 let add2NumbersUrl = "";
+
+add2NumsSubBtn.addEventListener("click", function(){
+    addApi(num1Input, num2Input);
+});
 
 function urlCall(url){
     fetch(url).then(
         response => response.text()
     ).then(
         data => {
+            add2NumsReturn.textContent = data;
             console.log(data);
-            alert(data);
+            //alert(data);
         }
     );
 }
 
 function addApi(num1Input, num2Input){
-    num1 = num1Input;
-    num2 = num2Input;
-    add2NumbersUrl = "https://kfujimuraallforonesprintapi.azurewebsites.net/MiniCh2/Add2Numbers/" + num1 + "/" + num2;
-    urlCall(add2NumbersUrl);
+    var letters = /^-?\d+$/;
+    if (num1Input.value.match(letters) && num2Input.value.match(letters)){
+        num1 = num1Input.value;
+        num2 = num2Input.value;
+        add2NumbersUrl = "https://kfujimuraallforonesprintapi.azurewebsites.net/MiniCh2/Add2Numbers/" + num1 + "/" + num2;
+        urlCall(add2NumbersUrl);
+    } else {
+        add2NumsReturn.textContent = "Enter a valid response. (Hint: Use only whole numbers.)";
+    }
 }
 
-addApi(4, 5);
+//addApi(4, 5);

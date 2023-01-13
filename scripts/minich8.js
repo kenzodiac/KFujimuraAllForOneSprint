9693bde8d81be2927e6149bdb5d2066d346d3477
kenzodiac <kennethfujimura@gmail.com>
2023-01-12 23:47:58 -0800

    finish all js; minor tweaks

--- scripts/minich8.js
+++ scripts/minich8.js
@@ -1,11 +1,30 @@
+let stringInput = document.getElementById("directoryInput");
+let firstNameReturn = document.getElementById("firstName");
+let lastNameReturn = document.getElementById("lastName");
+let slackNameReturn = document.getElementById("slackName");
+let emailReturn = document.getElementById("email");
+let hobbiesReturn = document.getElementById("hobbies");
+let directorySubBtn = document.getElementById("submit");
+
 let savedInput = "";
 let directoryUrl = "";
 
+let studentNames = [];
+
+directorySubBtn.addEventListener("click", function(){
+    directoryApi(stringInput);
+});
+
 function urlCall(url){
     fetch(url).then(
-        response => response.text()
+        response => response.json()
     ).then(
         data => {
+            firstNameReturn.textContent = data.firstName;
+            lastNameReturn.textContent = data.lastName;
+            slackNameReturn.textContent = data.slackName;
+            emailReturn.textContent = data.email;
+            hobbiesReturn.textContent = data.hobbies;
             console.log(data);
             //alert(data);
         }
@@ -13,9 +32,17 @@ function urlCall(url){
 }
 
 function directoryApi(input){
-    savedInput = input;
-    directoryUrl = "https://kfujimuraallforonesprintapi.azurewebsites.net/MiniCh8/GetStudentByAnything/" + savedInput;
-    urlCall(directoryUrl);
+    if (stringInput.value) {
+        savedInput = stringInput.value;
+        directoryUrl = "https://kfujimuraallforonesprintapi.azurewebsites.net/MiniCh8/GetStudentByAnything/" + savedInput;
+        urlCall(directoryUrl);
+    } else {
+        firstNameReturn.textContent = "N/A";
+        lastNameReturn.textContent = "N/A";
+        slackNameReturn.textContent = "N/A";
+        emailReturn.textContent = "N/A";
+        hobbiesReturn.textContent = "Make sure to enter a value in the input field!";
+    }
 }
 
-directoryApi('Kenneth');
+//directoryApi('Kenneth');
--- scripts/minich9.js
+++ scripts/minich9.js
@@ -1,11 +1,31 @@
+let choice1btn = document.getElementById("submit1");
+let choice2btn = document.getElementById("submit2");
+let choice3btn = document.getElementById("submit3");
+let choice4btn = document.getElementById("submit4");
+let restaurantReturn = document.getElementById("restaurantReturn");
+
 let savedInput = "";
 let restaurantUrl = "";
+
+choice1btn.addEventListener("click", function(){
+    restaurantPickerApi(1);
+});
+choice2btn.addEventListener("click", function(){
+    restaurantPickerApi(2);
+});
+choice3btn.addEventListener("click", function(){
+    restaurantPickerApi(3);
+});
+choice4btn.addEventListener("click", function(){
+    restaurantPickerApi(4);
+});
 
 function urlCall(url){
     fetch(url).then(
         response => response.text()
     ).then(
         data => {
+            restaurantReturn.textContent = data;
             console.log(data);
             //alert(data);
         }
@@ -18,4 +38,4 @@ function restaurantPickerApi(input){
     urlCall(restaurantUrl);
 }
 
-restaurantPickerApi(4);
+//restaurantPickerApi(4);
